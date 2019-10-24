b195f402cdf6273c3dd9aa8f5f133690bf7fa18a
Jared Minch <jxminch@gmail.com>
2019-10-24 15:27:27 -0700

    commit on [gh-pages]

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,20 @@
-/* Dummy service worker, required to enable installability. */
-self.addEventListener('fetch', function(event) {});
+/* Based largely on
+ * https://developers.google.com/web/fundamentals/codelabs/offline */
+
+/* Try fetching from the network first.  If that succeeds, cache the
+ * response.  If it fails, then get the data from the cache. */
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      var clone = response.clone();
+      console.log("Caching data for ", event.request.url);
+      caches.open('jabber').then(function(cache) {
+        cache.put(event.request, clone); });
+      return response;
+    })
+    .catch(function(e) {
+      console.log("Using cache for ", event.request.url);
+      return caches.match(event.request);
+    })
+  );
+});
