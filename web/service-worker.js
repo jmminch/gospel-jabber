2eca029a1c196cc4856d55a982598ca25c233210
Jared Minch <jxminch@gmail.com>
2019-10-25 11:05:51 -0700

    Do not cache data for other domains.

--- web/service-worker.js
+++ web/service-worker.js
@@ -6,10 +6,15 @@
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).then(function(response) {
-      var clone = response.clone();
-      console.log("Caching data for ", event.request.url);
-      caches.open('jabber').then(function(cache) {
-        cache.put(event.request, clone); });
+
+      /* Only want to cache data coming from this domain. */
+      if(parseUri(response.url).host === location.hostname) {
+        var clone = response.clone();
+        console.log("Caching data for ", event.request.url);
+        caches.open('jabber').then(function(cache) {
+          cache.put(event.request, clone); });
+      }
+
       return response;
     })
     .catch(function(e) {
@@ -18,3 +23,36 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+
+// parseUri 1.2.2
+// (c) Steven Levithan <stevenlevithan.com>
+// MIT License
+
+function parseUri (str) {
+	var	o   = parseUri.options,
+		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+		uri = {},
+		i   = 14;
+
+	while (i--) uri[o.key[i]] = m[i] || "";
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+		if ($1) uri[o.q.name][$1] = $2;
+	});
+
+	return uri;
+};
+
+parseUri.options = {
+	strictMode: false,
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	q:   {
+		name:   "queryKey",
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser: {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
